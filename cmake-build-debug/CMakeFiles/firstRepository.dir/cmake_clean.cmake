c872c2cce45299c87b76a7fc670f4949cdc2b497
MastixPower <mastixpower98@gmail.com>
2018-11-21 23:04:27 +0100

    pierwszy commit

---
+++ cmake-build-debug/CMakeFiles/firstRepository.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/firstRepository.dir/main.cpp.obj"
+  "firstRepository.pdb"
+  "firstRepository.exe"
+  "firstRepository.exe.manifest"
+  "libfirstRepository.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/firstRepository.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
